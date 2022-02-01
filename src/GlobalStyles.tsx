536dc76874cc9a92e13ec588e5e7fce3b51f1a94
sepehrdad <sepehrdad_kashiri@yahoo.com>
2022-02-02 01:18:45 +0330

    first commit.setted up project and made curser

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`   
+    body{
+        background-color: #212121;
+    }
+    *{
+        cursor: none;
+    }
+`;
+export default GlobalStyles;
---
+++ src/components/Curser.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+
+const Curser: React.FC = () => {
+  const [curserTop, setCurserTop] = useState("300px");
+  const [curserLeft, setCurserLeft] = useState("500px");
+  const curserPositionHandler = (e: MouseEvent) => {
+    setCurserTop(`${e.clientY - 40}px`);
+    setCurserLeft(`${e.clientX - 40}px`);
+  };
+  useEffect(() => {
+    document.addEventListener("mousemove", curserPositionHandler);
+    return () => {
+      document.removeEventListener("mousemove", curserPositionHandler);
+    };
+  });
+
+  return <PageCurser x={curserLeft} y={curserTop} />;
+};
+
+//styles
+
+const PageCurser = styled.div<{ x: string; y: string }>`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  background-color: green;
+  position: fixed;
+  top: ${({ y }) =>
+    css`
+      ${y}
+    `};
+  left: ${({ x }) =>
+    css`
+      ${x}
+    `};
+`;
+
+export default Curser;
